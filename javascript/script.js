9710ead5e310e64d343b70fc87e7f5e4333ac1a3
Robertson Morales <moralesso152842@gmail.com>
2022-10-18 00:13:19 +0800

    added: modal

--- javascript/script.js
+++ javascript/script.js
@@ -7,3 +7,14 @@ $('input').on('click', playButtonSound);
 function playButtonSound(){
     $('#btn-click')[0].play();
 }
+
+$('#btn-join').on('click', function(){
+    $('.modal').css('display', 'flex');
+});
+/**
+ * Modal 
+ */
+
+$('#btn-close').on('click', function(){
+    $('.modal').hide(150);
+});
